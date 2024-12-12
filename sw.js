13b522e36f35e95e94f901878d255043e66480e6
vitus.hg <78075711+VitusHg@users.noreply.github.com>
2024-12-12 18:01:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installiert');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetch:', event.request.url);
+  });
+  
